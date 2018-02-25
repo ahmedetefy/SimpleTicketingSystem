092573fbae1a549b7c1730a9f4207b1f486b69c5
Ahmed Etefy <ahmed.etefy12@gmail.com>
2018-02-25 16:58:01 +0200

    Added test that tests if registered user can login

--- backend/projects/flask-jwt-auth/project/tests/test_auth.py
+++ backend/projects/flask-jwt-auth/project/tests/test_auth.py
@@ -17,7 +17,18 @@ def register_user(self, email, password):
     )
 
 
-class TestAuthBlueprint(BaseTestCase):
+def login_user(self, email, password):
+    return self.client.post(
+        '/auth/login',
+        data=json.dumps(dict(
+            email=email,
+            password=password
+        )),
+        content_type='application/json',
+    )
+
+
+class TestAuthRegisteration(BaseTestCase):
     def test_registration(self):
         """ Test for user registration """
         with self.client:
@@ -45,6 +56,30 @@ class TestAuthBlueprint(BaseTestCase):
                 data['message'] == 'User already exists. Please Log in.')
             self.assertTrue(response.content_type == 'application/json')
             self.assertEqual(response.status_code, 202)
+
+
+class TestAuthLogin(BaseTestCase):
+    def test_registered_user_login(self):
+        """ Test for login of registered-user login """
+        with self.client:
+            # user registration
+            resp_register = register_user(self, 'byrd@byrd.com', 'byrd')
+            data_register = json.loads(resp_register.data.decode())
+            self.assertTrue(data_register['status'] == 'success')
+            self.assertTrue(
+                data_register['message'] == 'Successfully registered.'
+            )
+            self.assertTrue(data_register['auth_token'])
+            self.assertTrue(resp_register.content_type == 'application/json')
+            self.assertEqual(resp_register.status_code, 201)
+            # registered user login
+            response = login_user(self, 'byrd@byrd.com', 'byrd')
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'success')
+            self.assertTrue(data['message'] == 'Successfully logged in.')
+            self.assertTrue(data['auth_token'])
+            self.assertTrue(response.content_type == 'application/json')
+            self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
