d46a8c0e7159795eb38aad6262c78658de9adfbd
Ahmed Etefy <ahmed.etefy12@gmail.com>
2018-02-25 18:16:28 +0200

    Added test to ensure that token is not black list right after decoding

--- backend/projects/flask-jwt-auth/project/tests/test_auth.py
+++ backend/projects/flask-jwt-auth/project/tests/test_auth.py
@@ -3,7 +3,7 @@ import unittest
 import time
 
 from project.server import db
-from project.server.models import User
+from project.server.models import User, BlacklistToken
 from project.tests.base import BaseTestCase
 
 
@@ -207,6 +207,48 @@ class TestAuthLogout(BaseTestCase):
                 self.assertTrue(
                     data['message'] ==
                     'Signature expired. Please log in again.')
+                self.assertEqual(response.status_code, 401)
+
+        def test_valid_blacklisted_token_logout(self):
+            """ Test for logout after a valid token gets blacklisted """
+            with self.client:
+                # user registration
+                resp_register = register_user(self, "byrd@byrd.com", "byrd")
+                data_register = json.loads(resp_register.data.decode())
+                self.assertTrue(data_register['status'] == 'success')
+                self.assertTrue(
+                    data_register['message'] == 'Successfully registered.')
+                self.assertTrue(data_register['auth_token'])
+                self.assertTrue(resp_register.content_type ==
+                                'application/json')
+                self.assertEqual(resp_register.status_code, 201)
+                # user login
+                resp_login = login_user(self, "byrd@byrd.com", "byrd")
+                data_login = json.loads(resp_login.data.decode())
+                self.assertTrue(data_login['status'] == 'success')
+                self.assertTrue(data_login['message'] ==
+                                'Successfully logged in.')
+                self.assertTrue(data_login['auth_token'])
+                self.assertTrue(resp_login.content_type == 'application/json')
+                self.assertEqual(resp_login.status_code, 200)
+                # blacklist a valid token
+                blacklist_token = BlacklistToken(
+                    token=json.loads(resp_login.data.decode())['auth_token'])
+                db.session.add(blacklist_token)
+                db.session.commit()
+                # blacklisted valid token logout
+                response = self.client.post(
+                    '/auth/logout',
+                    headers=dict(
+                        Authorization='Bearer ' + json.loads(
+                            resp_login.data.decode()
+                        )['auth_token']
+                    )
+                )
+                data = json.loads(response.data.decode())
+                self.assertTrue(data['status'] == 'fail')
+                self.assertTrue(data['message'] ==
+                                'Token blacklisted. Please log in again.')
                 self.assertEqual(response.status_code, 401)
 
 
