7dedf863bb57baefdc1c46da46653c1f1232464b
Ahmed Etefy <ahmed.etefy12@gmail.com>
2018-02-25 21:14:01 +0200

    Added login and register functions to Auth Service

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class AuthService {
+  private BASE_URL: string = 'http://localhost:8000/auth';
+  private headers: Headers = new Headers({'Content-Type': 'application/json'});
 
-  constructor() { }
+  constructor(private http: Http) {}
 
-  test(): string {
-  	return "It is working"
+  login(user): Promise<any> {
+    let url: string = `${this.BASE_URL}/login`;
+    return this.http.post(url, user, {headers: this.headers}).toPromise();
   }
 
+  register(user): Promise<any> {
+    let url: string = `${this.BASE_URL}/register`;
+    return this.http.post(url, user, {headers: this.headers}).toPromise();
+  }
+  
 }
