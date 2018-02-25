19e8e1cba0ca8ee02671ff20da1674e92446369e
Ahmed Etefy <ahmed.etefy12@gmail.com>
2018-02-25 16:44:51 +0200

    Added test for Registeration functionality in test_user_model.py

--- backend/projects/flask-jwt-auth/project/tests/test_auth.py
+++ backend/projects/flask-jwt-auth/project/tests/test_auth.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 
 from project.server import db
@@ -5,8 +6,28 @@ from project.server.models import User
 from project.tests.base import BaseTestCase
 
 
+def register_user(self, email, password):
+    return self.client.post(
+        '/auth/register',
+        data=json.dumps(dict(
+            email=email,
+            password=password
+        )),
+        content_type='application/json',
+    )
+
+
 class TestAuthBlueprint(BaseTestCase):
-    pass
+    def test_registration(self):
+        """ Test for user registration """
+        with self.client:
+            response = register_user(self, 'joe@gmail.com', '123456')
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'success')
+            self.assertTrue(data['message'] == 'Successfully registered.')
+            self.assertTrue(data['auth_token'])
+            self.assertTrue(response.content_type == 'application/json')
+            self.assertEqual(response.status_code, 201)
 
 
 if __name__ == '__main__':
