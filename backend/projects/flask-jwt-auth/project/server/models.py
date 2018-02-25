d1bf772b0d113b3e83e9240805f698c0a88e7b03
Ahmed Etefy <ahmed.etefy12@gmail.com>
2018-02-25 16:33:42 +0200

    Added auth_decode method to Model:User and test_decode_auth in test_user_models.py

--- backend/projects/flask-jwt-auth/project/server/models.py
+++ backend/projects/flask-jwt-auth/project/server/models.py
@@ -43,3 +43,18 @@ class User(db.Model):
             )
         except Exception as e:
             return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
--- backend/projects/flask-jwt-auth/project/tests/test_user_model.py
+++ backend/projects/flask-jwt-auth/project/tests/test_user_model.py
@@ -17,6 +17,17 @@ class TestUserModel(BaseTestCase):
         auth_token = user.encode_auth_token(user.id)
         self.assertTrue(isinstance(auth_token, bytes))
 
+    def test_decode_auth_token(self):
+        user = User(
+            email='test@test.com',
+            password='test'
+        )
+        db.session.add(user)
+        db.session.commit()
+        auth_token = user.encode_auth_token(user.id)
+        self.assertTrue(isinstance(auth_token, bytes))
+        self.assertTrue(User.decode_auth_token(auth_token) == 1)
+
 
 if __name__ == '__main__':
     unittest.main()
