0b5950c1bd407e6f48bc96e1874df240188066cb
Ahmed Etefy <ahmed.etefy12@gmail.com>
2018-02-25 16:28:40 +0200

    Added model User and its corresponding TestCase

---
+++ backend/projects/flask-jwt-auth/project/tests/test_user_model.py
@@ -0,0 +1,22 @@
+import unittest
+
+from project.server import db
+from project.server.models import User
+from project.tests.base import BaseTestCase
+
+
+class TestUserModel(BaseTestCase):
+
+    def test_encode_auth_token(self):
+        user = User(
+            email='test@test.com',
+            password='test'
+        )
+        db.session.add(user)
+        db.session.commit()
+        auth_token = user.encode_auth_token(user.id)
+        self.assertTrue(isinstance(auth_token, bytes))
+
+
+if __name__ == '__main__':
+    unittest.main()
