a42e274fe4d5f224353fc0650486ac27fe922332
Ahmed Etefy <ahmed.etefy12@gmail.com>
2018-02-25 14:32:50 +0200

    Added Login Component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
   	AppRoutingModule,
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -1,12 +1,17 @@
 import {NgModule} 			   from '@angular/core';
 import {RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 const appRoutes: Routes = [
 	{
         path:"",
         component: HomeComponent,
+    },
+    {
+        path:"login",
+        component: LoginComponent,
     }
 ]
 
