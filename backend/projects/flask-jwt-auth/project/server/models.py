467c293e0b59342e1722a13e045648fa1ed2a527
Ahmed Etefy <ahmed.etefy12@gmail.com>
2018-02-25 22:37:29 +0200

    Updated expiry time in authentication token

--- backend/projects/flask-jwt-auth/project/server/models.py
+++ backend/projects/flask-jwt-auth/project/server/models.py
@@ -32,7 +32,7 @@ class User(db.Model):
         try:
             payload = {
                 'exp': (datetime.datetime.utcnow() +
-                        datetime.timedelta(days=0, seconds=5)),
+                        datetime.timedelta(days=1, seconds=5)),
                 'iat': datetime.datetime.utcnow(),
                 'sub': user_id
             }
