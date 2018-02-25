dd042d2e5a4ebb9ef29ff49e3b0df806c9967ac1
Ahmed Etefy <ahmed.etefy12@gmail.com>
2018-02-25 16:27:12 +0200

    Added SECRET KEY for hashing and Added tests to ensure variable is working properly

---
+++ backend/projects/flask-jwt-auth/project/server/models.py
@@ -0,0 +1,45 @@
+# project/server/models.py
+
+import jwt
+import datetime
+
+from project.server import app, db, bcrypt
+
+
+class User(db.Model):
+    """ User Model for storing user related details """
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __init__(self, email, password, admin=False):
+        self.email = email
+        self.password = bcrypt.generate_password_hash(
+            password, app.config.get('BCRYPT_LOG_ROUNDS')
+        ).decode()
+        self.registered_on = datetime.datetime.now()
+        self.admin = admin
+
+    def encode_auth_token(self, user_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': (datetime.datetime.utcnow() +
+                        datetime.timedelta(days=0, seconds=5)),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                app.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
--- backend/projects/flask-jwt-auth/project/tests/test__config.py
+++ backend/projects/flask-jwt-auth/project/tests/test__config.py
@@ -15,6 +15,7 @@ class TestDevelopmentConfig(TestCase):
         return app
 
     def test_app_is_development(self):
+        self.assertFalse(app.config['SECRET_KEY'] is 'my_precious')
         self.assertTrue(app.config['DEBUG'] is True)
         self.assertFalse(current_app is None)
         self.assertTrue(
@@ -29,6 +30,7 @@ class TestTestingConfig(TestCase):
         return app
 
     def test_app_is_testing(self):
+        self.assertFalse(app.config['SECRET_KEY'] is 'my_precious')
         self.assertTrue(app.config['DEBUG'])
         self.assertTrue(
             app.config['SQLALCHEMY_DATABASE_URI'] ==
