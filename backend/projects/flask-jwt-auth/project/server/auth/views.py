ca1e743c973503c8f3ad25fe526a5ea75dabb408
Ahmed Etefy <ahmed.etefy12@gmail.com>
2018-02-25 16:45:38 +0200

    Added view:RegisterAPI in server/auth/views.py

--- backend/projects/flask-jwt-auth/project/server/auth/views.py
+++ backend/projects/flask-jwt-auth/project/server/auth/views.py
@@ -5,3 +5,55 @@ from project.server import bcrypt, db
 from project.server.models import User
 
 auth_blueprint = Blueprint('auth', __name__)
+
+
+class RegisterAPI(MethodView):
+    """
+    User Registration Resource
+    """
+
+    def post(self):
+        # get the post data
+        post_data = request.get_json()
+        # check if user already exists
+        user = User.query.filter_by(email=post_data.get('email')).first()
+        if not user:
+            try:
+                user = User(
+                    email=post_data.get('email'),
+                    password=post_data.get('password')
+                )
+
+                # insert the user
+                db.session.add(user)
+                db.session.commit()
+                # generate the auth token
+                auth_token = user.encode_auth_token(user.id)
+                responseObject = {
+                    'status': 'success',
+                    'message': 'Successfully registered.',
+                    'auth_token': auth_token.decode()
+                }
+                return make_response(jsonify(responseObject)), 201
+            except Exception as e:
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'Some error occurred. Please try again.'
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'User already exists. Please Log in.',
+            }
+            return make_response(jsonify(responseObject)), 202
+
+# define the API resources
+registration_view = RegisterAPI.as_view('register_api')
+
+# add Rules for API Endpoints
+auth_blueprint.add_url_rule(
+    '/auth/register',
+    view_func=registration_view,
+    methods=['POST']
+)
