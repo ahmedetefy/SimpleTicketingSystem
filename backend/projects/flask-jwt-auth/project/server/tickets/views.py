bffcb7bd862ae6d43791631c7b859ca83813354d
Ahmed Etefy <ahmed.etefy12@gmail.com>
2018-02-26 15:38:59 +0200

    Added ListTicketAPI and its corresponding tests

--- backend/projects/flask-jwt-auth/project/server/tickets/views.py
+++ backend/projects/flask-jwt-auth/project/server/tickets/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, make_response, jsonify
 from flask.views import MethodView
 
 from project.server import bcrypt, db
-from project.server.models import Ticket
+from project.server.models import Ticket, User
 
 tickets_blueprint = Blueprint('tickets', __name__)
 
@@ -35,8 +35,52 @@ class CreateTicketAPI(MethodView):
             return make_response(jsonify(responseObject)), 401
 
 
+class ListTicketAPI(MethodView):
+    def get(self):
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            try:
+                auth_token = auth_header.split(" ")[1]
+            except IndexError:
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'Bearer token malformed.'
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = User.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                tickets = Ticket.query.all()
+                ticketlist = []
+                for ticket in tickets:
+                    tempTicket = {
+                        "name": ticket.name,
+                        "email": ticket.email,
+                        "subject": ticket.subject,
+                        "type": ticket.type,
+                        "urgency": ticket.urgency,
+                        "message": ticket.message
+                    }
+                    ticketlist.append(tempTicket)
+                return make_response(jsonify(ticketlist)), 201
+            responseObject = {
+                'status': 'fail',
+                'message': resp
+            }
+            return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return make_response(jsonify(responseObject)), 401
+
+
 # define the API resources
 creation_ticket_view = CreateTicketAPI.as_view('create_ticket_api')
+list_ticket_view = ListTicketAPI.as_view('list_ticket_api')
 
 
 # add Rules for API Endpoints
@@ -44,4 +88,9 @@ tickets_blueprint.add_url_rule(
     '/tickets/create',
     view_func=creation_ticket_view,
     methods=['POST']
+)
+tickets_blueprint.add_url_rule(
+    '/tickets/list',
+    view_func=list_ticket_view,
+    methods=['GET']
 )
--- backend/projects/flask-jwt-auth/project/tests/test_tickets.py
+++ backend/projects/flask-jwt-auth/project/tests/test_tickets.py
@@ -3,8 +3,30 @@ import json
 import unittest
 
 from project.server import db
-from project.server.models import Ticket
+from project.server.models import Ticket, BlacklistToken
 from project.tests.base import BaseTestCase
+
+
+def register_user(self, email, password):
+    return self.client.post(
+        '/auth/register',
+        data=json.dumps(dict(
+            email=email,
+            password=password
+        )),
+        content_type='application/json',
+    )
+
+
+def login_user(self, email, password):
+    return self.client.post(
+        '/auth/login',
+        data=json.dumps(dict(
+            email=email,
+            password=password
+        )),
+        content_type='application/json',
+    )
 
 
 class TestTicketCreation(BaseTestCase):
@@ -50,3 +72,90 @@ class TestTicketCreation(BaseTestCase):
                 data['message'] == 'Some error occurred. Please try again.')
             self.assertTrue(response.content_type == 'application/json')
             self.assertEqual(response.status_code, 401)
+
+
+class TestTicketListAPI(BaseTestCase):
+    def test_ticket_list_not_logged_in(self):
+        with self.client:
+            # user registration
+            resp_register = register_user(self, 'byrd@byrd.com', 'byrd')
+            data_register = json.loads(resp_register.data.decode())
+            self.assertTrue(data_register['status'] == 'success')
+            self.assertTrue(
+                data_register['message'] == 'Successfully registered.')
+            self.assertTrue(data_register['auth_token'])
+            self.assertTrue(resp_register.content_type == 'application/json')
+            self.assertEqual(resp_register.status_code, 201)
+            # blacklist a valid token
+            blacklist_token = BlacklistToken(
+                token=json.loads(resp_register.data.decode())['auth_token'])
+            db.session.add(blacklist_token)
+            db.session.commit()
+            response = self.client.get(
+                '/tickets/list',
+                headers=dict(
+                    Authorization='Bearer ' + json.loads(
+                        resp_register.data.decode()
+                    )['auth_token']
+                )
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'fail')
+
+    def test_ticket_list_logged_in(self):
+        with self.client:
+            # Add dummy tickets
+            self.client.post(
+                '/tickets/create',
+                data=json.dumps(dict(
+                    name="byrd",
+                    subject="Hello World",
+                    type="Bug Report",
+                    urgency="High",
+                    message="Testing",
+                    email="byrd@byrd.com"
+                )),
+                content_type='application/json',
+            )
+            self.client.post(
+                '/tickets/create',
+                data=json.dumps(dict(
+                    name="byrd2",
+                    subject="Hello World",
+                    type="Bug Report",
+                    urgency="High",
+                    message="Testing",
+                    email="byrd@byrd.com"
+                )),
+                content_type='application/json',
+            )
+            # user registration
+            resp_register = register_user(self, 'byrd@byrd.com', 'byrd')
+            data_register = json.loads(resp_register.data.decode())
+            self.assertTrue(data_register['status'] == 'success')
+            self.assertTrue(
+                data_register['message'] == 'Successfully registered.')
+            self.assertTrue(data_register['auth_token'])
+            self.assertTrue(resp_register.content_type == 'application/json')
+            self.assertEqual(resp_register.status_code, 201)
+            # user login
+            resp_login = login_user(self, 'byrd@byrd.com', 'byrd')
+            data_login = json.loads(resp_login.data.decode())
+            self.assertTrue(data_login['status'] == 'success')
+            self.assertTrue(data_login['message'] == 'Successfully logged in.')
+            self.assertTrue(data_login['auth_token'])
+            self.assertTrue(resp_login.content_type == 'application/json')
+            self.assertEqual(resp_login.status_code, 200)
+            #  Request ticket list
+            response = self.client.get(
+                '/tickets/list',
+                headers=dict(
+                    Authorization='Bearer ' + json.loads(
+                        resp_login.data.decode()
+                    )['auth_token']
+                )
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data[0]['name'] == 'byrd')
+            self.assertTrue(data[1]['name'] == 'byrd2')
+
